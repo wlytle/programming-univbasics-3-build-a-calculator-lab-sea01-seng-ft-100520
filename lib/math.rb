5d6cbd4d6a6a52eb59ef95176e050ca945847918
Will Lytle <wlytle89@gmail.com>
2020-08-11 18:57:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,13 @@
 def addition(num1, num2)
-
+  sum = num1 + num2
 end
 
 def subtraction(num1, num2)
-
+  difference = num1 - num2
 end
 
 def division(num1, num2)
-
+  quotient = num1/num2
 end
 
 def multiplication(num1, num2)
